e60a12006ced4db987a96510a64a423721769f66
twinbird827 <twinbird827@gmail.com>
2019-09-30 10:32:16 +0900

    [remove] 必要ないusing削除

--- Common/Util.cs
+++ Common/Util.cs
@@ -8,7 +8,6 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using WpfChartV1.Mvvm.UserControls;
 using WpfUtilV2.Common;
 using WpfUtilV2.Extensions;
 
